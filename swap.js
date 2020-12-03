0ecb0924428470087651d533d91b0da29bda0e59
Md.Dulal Miah <dulalmiah70112@gmail.com>
2020-12-04 04:14:28 +0600

    Done swap with temp and without temp

--- swap.js
+++ swap.js
@@ -1,3 +1,12 @@
 let a = 5 ; 
 let b = 7;
 console.log('Brfore Swap a = ',a ,'b = ',b);
+ [a , b] = [b, a];
+console.log('After Swap a = ',a ,'b = ',b);
+
+//use another way
+
+let temp = a;
+a = b;
+b = temp
+console.log('After Swap a = ',a ,'b = ',b);
