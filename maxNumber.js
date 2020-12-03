2af4fc01dee600c7dbe50f4807f673bade950ad4
Md.Dulal Miah <dulalmiah70112@gmail.com>
2020-12-04 04:56:28 +0600

    Find max number in three ways (consitional,method,ternary)

---
+++ maxNumber.js
@@ -0,0 +1,24 @@
+let dulalTaka = 450000;
+let hafizulTaka = 50000;
+let komolTaka = 300000;
+
+/*if((dulalTaka > hafizulTaka) && (dulalTaka > komolTaka)){
+    console.log('Dulal have lot of money');
+}
+else if(hafizulTaka > komolTaka){
+    console.log('Hafizul have lot of money');
+}
+else{
+    console.log('Komol have lot of money');
+}*/
+
+/*let mostAmount = Math.max(dulalTaka , hafizulTaka , komolTaka);
+console.log(mostAmount);*/
+
+//use ternary operator 
+
+let num1 = 12;
+let num2 = 16;
+
+const bigNumber = ( num1 > num2 ) ? num1 : num2;
+console.log(bigNumber);
