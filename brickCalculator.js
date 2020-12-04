d00f5e9aea325022165a827b3ca0842f9592fb39
Md.Dulal Miah <dulalmiah70112@gmail.com>
2020-12-04 18:07:22 +0600

    Done some issue

--- brickCalculator.js
+++ brickCalculator.js
@@ -1,4 +1,5 @@
 function brickCalculator (size){
+   if(size>0){
     if (size > 20){
         let brick = (size - 20)*10 + (10 * 12) + (10*15) ;
         return brick;
@@ -10,5 +11,9 @@ function brickCalculator (size){
     else {
         return size*15;
     }
+   }
+   else{
+       return 'Enter valid size'
+   }
 }
-console.log(brickCalculator(9));
+console.log(brickCalculator(-8));
