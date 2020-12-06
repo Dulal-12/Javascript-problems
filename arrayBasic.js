b3c9fcbaf2fb23f9f298826969ed93d396e5b420
Md.Dulal Miah <dulalmiah70112@gmail.com>
2020-12-07 04:31:00 +0600

    Done arrayIndexOf method

---
+++ arrayBasic.js
@@ -0,0 +1,8 @@
+var friendAge = [ 13 , 12, 56 , 56]; //position start from 0 , 1, ,2 , 3
+//friendAge[0] = 13
+//friendAge[1] = 12 ;
+//friendAge[2] = 56;
+friendAge[3] = 27 ;
+// console.log(friendAge);
+console.log(friendAge.indexOf(56));      //This method give me array value position . If value is not here , That is give me -1.
+console.log(friendAge.indexOf(11));
