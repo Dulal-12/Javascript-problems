98fc858060f34b42956b55a082221a71384bd643
Md.Dulal Miah <dulalmiah70112@gmail.com>
2020-12-07 04:41:25 +0600

    done push,pop,shift,unshift

---
+++ pushPop.js
@@ -0,0 +1,11 @@
+let arr = [1,2,4,4,5] ; 
+arr.push(67);
+arr.push(69);                              //push method add element in the last position and pop delete last element
+// console.log(arr);
+
+arr.pop();
+
+// console.log(arr);
+arr.shift();                                    //shift method delete first element in array
+arr.unshift(45);                           //unshift method add element
+console.log(arr);
