27ff272e92a06ebb3d8c1b3daccc607f27186ac2
Md.Dulal Miah <dulalmiah70112@gmail.com>
2020-12-07 05:02:29 +0600

    splice method change the main array

---
+++ splice.js
@@ -0,0 +1,9 @@
+// splice method ( start , deleteCount , addElement);
+let name = ['Dulal' , 'Ismail', 'Zilllur' , 'Sovon'];
+
+name.splice(1 , 2 , 'Ammu');
+console.log(name);
+
+let age = [ 'seven' , 'eight' , 'nine' , 'ten'] ; 
+age.splice(2, 2 , 'Eleven');
+console.log(age);
